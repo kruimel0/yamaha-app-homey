e4b2eca42315a74a33a359b5099f0bd427c6d744
kruimel0 <koen-martens@hotmail.com>
2017-02-13 23:12:44 +0100

    V0.2.5

--- drivers/receiver/driver.js
+++ drivers/receiver/driver.js
@@ -14,8 +14,13 @@ var autoupdate = 0;
 //Pairing information
 var devices = {};
 module.exports.init = function( devices_data, callback ) {
-    devices_data.forEach(function(device_data){
+	devices_data.forEach(function(device_data){
+	try{
         initDevice( device_data );
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
     })
 
     callback();
@@ -100,6 +105,7 @@ module.exports.pair = function (socket) {
 
 //When settings are changed
 module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj, changedKeysArr, callback ) {
+	try{
     // run when the user has changed the device's settings in Homey.
     // changedKeysArr contains an array of keys that have been changed, for your convenience :)
 
@@ -114,7 +120,10 @@ module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj,
 	Homey.log(newSettingsObj)
 	Homey.log(device_data)
     callback( null, true );
-
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
 }
 
 //Capabilities (i.e. mobile cards)
@@ -251,20 +260,29 @@ module.exports.capabilities = {
 
 // a helper method to get a device from the devices list by it's device_data object
 function getDeviceByData( device_data ) {
+	try{
     var device = devices[ device_data.id ];
     if( typeof device === 'undefined' ) {
         return new Error("invalid_device");
     } else {
         return device;
     }
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
 }
 // a helper method to add a device to the devices list
 function initDevice( device_data ) {
+	try{
 	Homey.log('Subfunction InitDevice')
     devices[ device_data.id ] = {};
     GetStatus(device_data);
     devices[ device_data.id ].data = device_data;
-
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
 }
 //Flow action handlers
 //Homey.manager('flow').on('action.getStatus', function (callback, args){
@@ -336,6 +354,7 @@ Homey.manager('flow').on('action.volumeDown', function (callback, args){
 
 //Set and get XML information
 function SendXMLToReceiver (ipaddress,xml){
+	try{
 	Homey.log ("Yamaha receiver app - sending " + xml + " to " + hostIP);
     var latestPostHTTP = request
 	.post({
@@ -350,8 +369,13 @@ function SendXMLToReceiver (ipaddress,xml){
 	})
 	Homey.log ("Lastest HTTP_send: %j",latestPostHTTP)	
 	Homey.log ('callback true');
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
 };
 function GetXMLFromReceiver (xml){
+	try{
 	Homey.log ("Yamaha receiver app - getting " + xml + " from " + hostIP);
     var latestGetHTTP = request
 	.post({
@@ -368,162 +392,173 @@ function GetXMLFromReceiver (xml){
     Homey.log('BODY: ' + chunk);
 	});
 	Homey.log ("Lastest HTTP_GET: %j",latestGetHTTP)
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
 };
 //Get status and parse it --> this is the XML part.
 function GetStatus (cap){
+	try{
 	if (typeof cap !== undefined){
-	xml = '<YAMAHA_AV cmd="GET"><Main_Zone><Basic_Status>GetParam</Basic_Status></Main_Zone></YAMAHA_AV>';
-	Homey.log ("Yamaha receiver app - status request " + xml + " from " + hostIP + 'with cap = ' +cap);
-    var latestGetHTTP = request
-	.post({
-        method: 'POST', 
-        uri: 'http://'+hostIP+'/YamahaRemoteControl/ctrl',
-        body:xml
-    })
-	.on('response', function(response) {
-		//Check if status is 200 (toDO)
-		Homey.log('Response status (200 is okay): '+response.statusCode) // 200
-	})
-	.on('data', function (chunk) {
-		Homey.log('Full BODY: ' + chunk);
-		
-		//Regexp the result - Mute
-		var str = String(chunk);
-		var regexp = /Lvl><Mute>((On|Off))<\/Mute/i;
-		var matches_array = str.match(regexp);
-		//Make muteStatus binary: 1 is on, 0 is off.
-		if (matches_array !== null ){
-			var muteStatus = matches_array[1].replace('On',1);
-			muteStatus = muteStatus.replace('Off',0);
-		}
-		
-		//Regexp the result - Volume value
-		var regexp = /<Volume><Lvl><Val>(.?([0-9])*)<\/Val/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var volumeStatus = matches_array[1];
-			//Set volumeStatus to 0-1 float value
-			volumeStatus=((volumeStatus/5)+160)/192;
-		}
+		xml = '<YAMAHA_AV cmd="GET"><Main_Zone><Basic_Status>GetParam</Basic_Status></Main_Zone></YAMAHA_AV>';
+		Homey.log ("Yamaha receiver app - status request " + xml + " from " + hostIP + 'with cap = ' +cap);
+		var latestGetHTTP = request
+		.post({
+			method: 'POST', 
+			uri: 'http://'+hostIP+'/YamahaRemoteControl/ctrl',
+			body:xml
+		})
+		.on('response', function(response) {
+			//Check if status is 200 (toDO)
+			Homey.log('Response status (200 is okay): '+response.statusCode) // 200
+		})
+		.on('data', function (chunk) {
+			Homey.log('Full BODY: ' + chunk);
+			
+			//Regexp the result - Mute
+			var str = String(chunk);
+			var regexp = /Lvl><Mute>((On|Off))<\/Mute/i;
+			var matches_array = str.match(regexp);
+			//Make muteStatus binary: 1 is on, 0 is off.
+			if (matches_array !== null ){
+				var muteStatus = matches_array[1].replace('On',1);
+				muteStatus = muteStatus.replace('Off',0);
+			}
+			
+			//Regexp the result - Volume value
+			var regexp = /<Volume><Lvl><Val>(.?([0-9])*)<\/Val/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var volumeStatus = matches_array[1];
+				//Set volumeStatus to 0-1 float value
+				volumeStatus=((volumeStatus/5)+160)/192;
+			}
+					
+			//Regexp the result - current input
+			var regexp = /<Input><Input_Sel>(.*?)<\/Input_Sel>/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var inputStatus = matches_array[1];
+			}
+			
+			//Regexp the result - sourceName
+			var regexp = /<Src_Name>(.*?)<\/Src_Name>/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var sourceNameStatus = matches_array[1];
+			}
+			
+			//Regexp the result - onoff state
+			var regexp = /Power_Control><Power>(.*?)<\/Power>/i;
+			var matches_array = str.match(regexp);
+			//Make onoffStatus binary: 1 is on, 0 is off.
+			if (matches_array !== null ){
+				var onoffStatus = matches_array[1].replace('On',1);
+				onoffStatus = onoffStatus.replace('Off',0);
+			}
+			
+			//Regexp the result - sleep state
+			var regexp = /Power><Sleep>(.*?)<\/Sleep>/i;
+			var matches_array = str.match(regexp);
+			//Make sleepStatus binary: 1 is on, 0 is off.
+			if (matches_array !== null ){
+				var sleepStatus = matches_array[1].replace('On',1);
+				sleepStatus = sleepStatus.replace('Off',0);
+			}
+			
+			//Regexp the result - straight option
+			var regexp = /<Straight>(.*?)<\/Straight>/i;
+			var matches_array = str.match(regexp);
+			//Make straightStatus binary: 1 is on, 0 is off.
+			if (matches_array !== null ){
+				var straightStatus = matches_array[1].replace('On',1);
+				straightStatus = straightStatus.replace('Off',0);
+			}
+			
+			//Regexp the result - enhancer option
+			var regexp = /<Enhancer>(.*?)<\/Enhancer>/i;
+			var matches_array = str.match(regexp);
+			//Make straightStatus binary: 1 is on, 0 is off.
+			if (matches_array !== null ){
+				var enhancerStatus = matches_array[1].replace('On',1);
+				enhancerStatus = enhancerStatus.replace('Off',0);
+			}
+			
+			//Regexp the result - soundProgram
+			var regexp = /<Sound_Program>(.*?)<\/Sound_Program>/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var soundProgramStatus = matches_array[1];
+			}
+			
+			//Regexp the result - bassValue
+			var regexp = /<Bass><Val>(.*?)<\/Val>/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var bassValueStatus = matches_array[1];
+			}
+			
+			//Regexp the result - trebleValue
+			var regexp = /<Treble><Val>(.*?)<\/Val>/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var trebleValueStatus = matches_array[1];
+			}
+			
+			//Test regexp which is wrong forsure
+			var regexp = /<Trebless><Val>(.*?)<\/Val>/i;
+			var matches_array = str.match(regexp);
+			if (matches_array !== null ){
+				var trebleValueStatus2 = matches_array[1];
+			}
+			Homey.log("--------------------------------- Error avoided!")
+			
+			//Give a log of the results
+			Homey.log('---------------------------------------------')
+			Homey.log('Mute Status: '+muteStatus);
+			Homey.log('Volume Status: '+volumeStatus);
+			Homey.log('Input Status: '+inputStatus);
+			Homey.log('Onoff Status: '+onoffStatus);
+			Homey.log('Sleep Status: '+sleepStatus);
+			Homey.log('Source Name Status: '+sourceNameStatus);
+			Homey.log('Straight Status: '+straightStatus);
+			Homey.log('Enhancer Status: '+enhancerStatus);
+			Homey.log('Sound Program Status: '+soundProgramStatus);
+			Homey.log('Bass value Status: '+bassValueStatus);
+			Homey.log('Treble value Status: '+trebleValueStatus);
+			Homey.log('Wrong value test: '+trebleValueStatus2);
+			
+			//Capabilities part
+			//Changing all capabilities - too much, but no workaround at the moment
+			if (cap !== 0) {
+				//Homey.log('Capabilities part in GetStatus part %j - %j',cap,muteStatus)
+				//var device = getDeviceByData( cap )
+				//Homey.log('Current volume_mute status: '+device)
+				if (muteStatus == 0){module.exports.realtime( cap, 'volume_mute', false)}
+				else{module.exports.realtime( cap, 'volume_mute', true)}
 				
-		//Regexp the result - current input
-		var regexp = /<Input><Input_Sel>(.*?)<\/Input_Sel>/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var inputStatus = matches_array[1];
-		}
-		
-		//Regexp the result - sourceName
-		var regexp = /<Src_Name>(.*?)<\/Src_Name>/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var sourceNameStatus = matches_array[1];
-		}
-		
-		//Regexp the result - onoff state
-		var regexp = /Power_Control><Power>(.*?)<\/Power>/i;
-		var matches_array = str.match(regexp);
-		//Make onoffStatus binary: 1 is on, 0 is off.
-		if (matches_array !== null ){
-			var onoffStatus = matches_array[1].replace('On',1);
-			onoffStatus = onoffStatus.replace('Off',0);
-		}
-		
-		//Regexp the result - sleep state
-		var regexp = /Power><Sleep>(.*?)<\/Sleep>/i;
-		var matches_array = str.match(regexp);
-		//Make sleepStatus binary: 1 is on, 0 is off.
-		if (matches_array !== null ){
-			var sleepStatus = matches_array[1].replace('On',1);
-			sleepStatus = sleepStatus.replace('Off',0);
-		}
-		
-		//Regexp the result - straight option
-		var regexp = /<Straight>(.*?)<\/Straight>/i;
-		var matches_array = str.match(regexp);
-		//Make straightStatus binary: 1 is on, 0 is off.
-		if (matches_array !== null ){
-			var straightStatus = matches_array[1].replace('On',1);
-			straightStatus = straightStatus.replace('Off',0);
-		}
-		
-		//Regexp the result - enhancer option
-		var regexp = /<Enhancer>(.*?)<\/Enhancer>/i;
-		var matches_array = str.match(regexp);
-		//Make straightStatus binary: 1 is on, 0 is off.
-		if (matches_array !== null ){
-			var enhancerStatus = matches_array[1].replace('On',1);
-			enhancerStatus = enhancerStatus.replace('Off',0);
-		}
-		
-		//Regexp the result - soundProgram
-		var regexp = /<Sound_Program>(.*?)<\/Sound_Program>/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var soundProgramStatus = matches_array[1];
-		}
-		
-		//Regexp the result - bassValue
-		var regexp = /<Bass><Val>(.*?)<\/Val>/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var bassValueStatus = matches_array[1];
-		}
-		
-		//Regexp the result - trebleValue
-		var regexp = /<Treble><Val>(.*?)<\/Val>/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var trebleValueStatus = matches_array[1];
-		}
-		
-		//Test regexp which is wrong forsure
-		var regexp = /<Trebless><Val>(.*?)<\/Val>/i;
-		var matches_array = str.match(regexp);
-		if (matches_array !== null ){
-			var trebleValueStatus2 = matches_array[1];
-		}
-		Homey.log("--------------------------------- Error avoided!")
-		
-		//Give a log of the results
-		Homey.log('---------------------------------------------')
-		Homey.log('Mute Status: '+muteStatus);
-		Homey.log('Volume Status: '+volumeStatus);
-		Homey.log('Input Status: '+inputStatus);
-		Homey.log('Onoff Status: '+onoffStatus);
-		Homey.log('Sleep Status: '+sleepStatus);
-		Homey.log('Source Name Status: '+sourceNameStatus);
-		Homey.log('Straight Status: '+straightStatus);
-		Homey.log('Enhancer Status: '+enhancerStatus);
-		Homey.log('Sound Program Status: '+soundProgramStatus);
-		Homey.log('Bass value Status: '+bassValueStatus);
-		Homey.log('Treble value Status: '+trebleValueStatus);
-		Homey.log('Wrong value test: '+trebleValueStatus2);
-		
-		//Capabilities part
-		//Changing all capabilities - too much, but no workaround at the moment
-		if (cap !== 0) {
-			//Homey.log('Capabilities part in GetStatus part %j - %j',cap,muteStatus)
-			//var device = getDeviceByData( cap )
-			//Homey.log('Current volume_mute status: '+device)
-			if (muteStatus == 0){module.exports.realtime( cap, 'volume_mute', false)}
-			else{module.exports.realtime( cap, 'volume_mute', true)}
+				if (onoffStatus == 0){module.exports.realtime( cap, 'onoff', false)}
+				else{module.exports.realtime( cap, 'onoff', true)}
+				
+				if (typeof inputStatus !== undefined){
+					module.exports.realtime( cap, 'source_selected', String(inputStatus))
+				} else {module.exports.realtime( cap, 'source_selected', "Nothing found")}
+				if (typeof soundProgramStatus !== undefined){
+					module.exports.realtime( cap, 'soundprogram_selected', String(soundProgramStatus))
+				} else {module.exports.realtime( cap, 'soundprogram_selected', "Nothing found")}
+				if (typeof volumeStatus !== undefined){
+					module.exports.realtime( cap, 'volume_set', volumeStatus)
+				} else {module.exports.realtime( cap, 'volume_set', 0)}
+			};
 			
-			if (onoffStatus == 0){module.exports.realtime( cap, 'onoff', false)}
-			else{module.exports.realtime( cap, 'onoff', true)}
-			
-			if (typeof inputStatus !== undefined){
-				module.exports.realtime( cap, 'source_selected', String(inputStatus))
-			} else {module.exports.realtime( cap, 'source_selected', "Nothing found")}
-			if (typeof soundProgramStatus !== undefined){
-				module.exports.realtime( cap, 'soundprogram_selected', String(soundProgramStatus))
-			} else {module.exports.realtime( cap, 'soundprogram_selected', "Nothing found")}
-			if (typeof volumeStatus !== undefined){
-				module.exports.realtime( cap, 'volume_set', volumeStatus)
-			} else {module.exports.realtime( cap, 'volume_set', 0)}
-		};
 	});
-  }
+	
+	}
+	}
+	catch (e){
+		//Nothing here, just catching errors
+	}
 };
 //Autocomplete part
 var allPossibleInputs = [
